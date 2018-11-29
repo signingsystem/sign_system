2f75fe7e1d6be7e7177d14dff6ac44cda732cf39
zjy1992518 <474438417@qq.com>
2018-11-29 19:40:08 +0800

    签到成功总天数页面

---
+++ app/src/main/java/com/example/administrator/signsystem/SignSuccessfulActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.signsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignSuccessfulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_successful);
+    }
+}
